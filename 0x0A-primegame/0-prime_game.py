6e9e696546ec7b05e2f00897a103e6edf098c420
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-04-05 00:51:20 +0100

    go

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+""" Prime Game Algorithm Python """
+
+
 def createList(y):
+    """ Creates a list for the set of numbers"""
     return [x for x in range(1, y + 1)]
 
 
 def isPrime(x):
+    """ Checks if a number given n is a prime number """
     if x >= 2:
         for n in range(2, x):
             if (x % n) == 0:
@@ -15,6 +20,7 @@ def isPrime(x):
 
 
 def numOfPrime(x):
+    """ Calculate all primes """
     num_prime = 0
     for i in range(0, len(x)):
         if isPrime(x[i]):
@@ -23,6 +29,12 @@ def numOfPrime(x):
 
 
 def isWinner(x, nums):
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
     bwin = 0
     mwin = 0
 
